e930b529f952c59d279ace177f60cedaaeec9f5c
cunshu <1498645398@qq.com>
2017-04-10 15:35:32 +0800

    add

--- js/common.js
+++ js/common.js
@@ -1,3 +1,46 @@
 $(function(){
-	
+
+	//二维码显示
+	$("#wechat").hover(function(){
+		$(this).find("span").show();
+	},function(){
+		$(this).find("span").hide();
+	});
+
+	//首页轮播图
+	var bannerImg = $(".banner ul li");
+	var bannerDl = $(".banner dl dd");
+	var num = 0,adTimer = null;
+	function bannerScroll(){
+		num++;
+		bannerImg.eq(num).show().siblings().hide();
+		bannerDl.eq(num).addClass("current").siblings().removeClass("current");
+		if (num == 3) {
+			num = -1;
+		}
+	};
+	var adTimer = setInterval(bannerScroll,2000);
+	$(".banner").hover(function(){
+		clearInterval(adTimer);
+	},function(){
+		adTimer = setInterval(bannerScroll,2000);
+	});
+
+	bannerDl.on("click",function(){
+		$(this).addClass("current").siblings().removeClass("current");
+		bannerImg.eq($(this).index()).show().siblings().hide();
+		num = $(this).index();
+		if (num == 3) num = -1;
+	});
+
+	//新闻列表页列表切换
+	$(".tab-list1-nav li").on("click",function(){
+		$(this).addClass("current").siblings().removeClass("current");
+		$(".tab-list1-content ul").eq($(this).index()).show().siblings().hide();
+	});
+	$(".tab-list3-nav li").on("click",function(){
+		$(this).addClass("current").siblings().removeClass("current");
+		$(".tab-list3-content ul").eq($(this).index()).show().siblings().hide();
+	});
+
 })
